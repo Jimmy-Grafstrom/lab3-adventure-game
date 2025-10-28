4af7a72712a7d70447284faf71d3f280068d106d
user.name=Jimmy Grafström <212706765+Jimmy-Grafstrom@users.noreply.github.com>
2025-10-28 22:02:00 +0100

    Feat/new-rooms MountainRoom done

---
+++ src/main/java/se/sprinto/hakan/adventuregame/model/MountainRoom.java
@@ -0,0 +1,18 @@
+package se.sprinto.hakan.adventuregame.model;
+
+import se.sprinto.hakan.adventuregame.view.UI;
+
+public class MountainRoom implements Room{
+    @Override
+    public void enterRoom(Player player, UI ui) {
+        ui.showMessage("Du står framför stora branta berg. Det ser livsfarligt ut.");
+        ui.showMessage("Du borde nog vända om, du behöver inte klättra för att klara spelet.");
+        String choice = ui.getInput("Vill du klättra i bergen ja/nej");
+        if (choice.equalsIgnoreCase("ja")) {
+            ui.showMessage("Du börjar klättra, men du faller ner för ett stup");
+            player.setHealth(0);
+        } else {
+            ui.showMessage("Klokt att vända om.");
+        }
+    }
+}
