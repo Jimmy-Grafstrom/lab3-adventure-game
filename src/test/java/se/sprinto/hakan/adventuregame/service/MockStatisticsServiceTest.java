5015482856a8ee29588d949f6ac86f1b0a080b81
user.name=Jimmy Grafström <212706765+Jimmy-Grafstrom@users.noreply.github.com>
2025-10-29 14:20:36 +0100

    test/FakeFileStatisticsDao mock test done

---
+++ src/test/java/se/sprinto/hakan/adventuregame/service/MockStatisticsServiceTest.java
@@ -0,0 +1,48 @@
+package se.sprinto.hakan.adventuregame.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.sprinto.hakan.adventuregame.dao.StatisticsDao;
+import se.sprinto.hakan.adventuregame.model.Statistics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MockStatisticsServiceTest {
+
+    @Mock
+    private StatisticsDao mockDao;
+    @InjectMocks
+    private StatisticsService statisticService;
+
+    private List<Statistics> getUnsortedList() {
+        return new ArrayList<>(List.of(
+                new Statistics("player-2nd", 100),
+                new Statistics("player-3rd", 20),
+                new Statistics("player-1st", 300)
+        ));
+    }
+
+    @Test
+    void getSortedStatistics() {
+        when(mockDao.loadAll()).thenReturn(getUnsortedList());
+        List<Statistics> sortedStats = statisticService.getSortedStatistics();
+
+        assertEquals(3, sortedStats.size());
+        assertEquals(300, sortedStats.get(0).getScore(), "Högsta poängen (300) ska vara först");
+        assertEquals("player-1st", sortedStats.get(0).getPlayerName());
+        assertEquals(100, sortedStats.get(1).getScore());
+        assertEquals(20, sortedStats.get(2).getScore());
+
+        verify(mockDao).loadAll();
+    }
+}
